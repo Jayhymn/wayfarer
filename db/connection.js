d5b6463308b29f21cf640262d82a895e56da5bd1
Adetunji Adegbite <tunjiadegbite@outlook.com>
2020-01-19 16:51:58 +0100

    modified connection file

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,14 @@
-// import { Client } from 'pg'
-// import dotenv from 'dotenv'
-
-const { Client } = require('pg')
-const dotenv = require('dotenv')
+import { Client } from 'pg'
+import dotenv from 'dotenv'
 
 dotenv.config()
 
 const client = new Client({
+    server: process.env.IP || '127.0.0.1',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DATABASE
 })
-client.connect()
+module.exports = client
