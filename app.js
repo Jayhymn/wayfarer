8756549f31aa912f4548edfb05ffb9712aa5023e
Adetunji Adegbite <tunjiadegbite@outlook.com>
2020-01-20 17:00:18 +0100

    porgrammed the entry point of the app

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import users from './routes/user'
+import trip from './routes/trip'
+import booking from './routes/booking'
 
+dotenv.config()
 const app = express()
-
 app.use(express.json())
+
+app.use('/api/v1/auth', users)
+
+app.use('/api/v1', trip)
+
+app.use('/api/v1', booking)
 
 const port = process.env.PORT || 3000
 app.listen(port)
+
+module.exports = app
