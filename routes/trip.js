b3c8bb756fa61ffb81c1e7698ef43c231a373e8e
Adetunji Adegbite <tunjiadegbite@outlook.com>
2020-01-20 08:20:52 +0100

    created routes to match urls related to trips with their respective handlers

---
+++ routes/trip.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import { check } from 'express-validator'
+import trip from '../controllers/trip'
+
+dotenv.config()
+
+const validation = [
+    check('is_admin').not().isEmpty().isBoolean(), check('bus_id').isAlphanumeric().withMessage('invalid bus id'),
+    check('origin').isAlphanumeric().withMessage('invalid address'), check('destination').isAlphanumeric().withMessage('invalid address'),
+    check('trip_date').isISO8601().withMessage('not a valid date'), check('fare').isFloat().withMessage('is not a valid amount'),
+  ]
+const router = express.Router()
+
+router.post('/trip', validation, trip.create_trip)
+router.get('/trip', trip.get_trips)
+
+module.exports = router
