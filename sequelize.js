c79a97cd07cde124b124ebf94a33df8be6b1bd65
Adetunji Adegbite <tunjiadegbite@outlook.com>
2020-02-07 18:05:41 +0100

    re-intsalled sequelize and modified sequelize.js file

--- sequelize.js
+++ sequelize.js
@@ -1,11 +1,14 @@
 import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv'
 
+// const { Sequelize } = require('sequelize').Sequelize
+// const dotenv = require('dotenv')
+
 dotenv.config()
 
 const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER,
      process.env.DB_PASSWORD, {
-         host: process.env.DB_PASSWORD,
+         host: process.env.DB_HOST,
          dialect: 'postgres',
 
         pool: {
@@ -14,6 +17,9 @@ const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER,
             idle: 10000
             }
      })
-
+    //  const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+    sequelize.authenticate()
+    .then(()=> console.log('sequelize connected successfully'))
+    .catch(e => console.error('unable to connect'))
 
 module.exports = sequelize
